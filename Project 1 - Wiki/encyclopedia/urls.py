8f349f6ec4f62d7bd66ea660e49b532c5268d534
antonio-hus <lucian.hus@stud.ubbcluj.ro>
2024-02-13 00:21:36 +0200

    Implemented Page Rendering

--- Project 1 - Wiki/encyclopedia/urls.py
+++ Project 1 - Wiki/encyclopedia/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:title>", views.read_entry, name="read_entry")
 ]
--- Project 1 - Wiki/encyclopedia/views.py
+++ Project 1 - Wiki/encyclopedia/views.py
@@ -1,10 +1,31 @@
+# Imports Section
+# --- Django Related Imports ---
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# --- UTILS ---
 from . import util
+
+# --- MISC ---
+from markdown2 import markdown
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def read_entry(request, title: str):
+    entry = util.get_entry(title)
+    if entry:
+        title = title.capitalize()
+        entry_html = markdown(entry)
+        return render(request, "encyclopedia/entry_page.html", {
+            "title": title,
+            "entry_html": entry_html
+        })
+    else:
+        return render(request, "encyclopedia/404_page.html", {
+            "title": title
+        })
 
