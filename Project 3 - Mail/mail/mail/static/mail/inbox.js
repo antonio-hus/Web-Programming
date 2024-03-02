e87849c8df0add4f2cfdab6b564aecf8453ec113
antonio-hus <lucian.hus@stud.ubbcluj.ro>
2024-03-02 19:07:23 +0200

    Email Send & Mailbox View Completed

--- Project 3 - Mail/mail/mail/static/mail/inbox.js
+++ Project 3 - Mail/mail/mail/static/mail/inbox.js
@@ -20,6 +20,28 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // On submit, make a POST request to /emails
+  document.querySelector('#compose-form').onsubmit = () => {
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value,
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
+
+    // Load the 'Sent' Mailbox
+    load_mailbox('sent')
+
+    return false;
+  }
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +52,35 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Show the mailbox content
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    emails.forEach(email => {
+      const email_block = document.createElement('div');
+      const left_side = document.createElement('div');
+      const right_side = document.createElement('div');
+      const sender = document.createElement('strong');
+      const subject = document.createElement('p');
+      const timestamp = document.createElement('div');
+      const reply_button = document.createElement('button');
+
+      sender.innerHTML = `${email.sender}`;
+      subject.innerHTML = `${email.subject}`;
+      timestamp.innerHTML = `${email.timestamp}`;
+      reply_button.innerHTML = 'Reply';
+      reply_button.className = "btn btn-sm btn-outline-primary";
+
+      left_side.appendChild(sender);
+      left_side.appendChild(subject);
+      right_side.appendChild(timestamp);
+      right_side.appendChild(reply_button);
+      email_block.appendChild(left_side);
+      email_block.appendChild(right_side);
+      email.read?email_block.className = 'email-preview-unread':email_block.className = 'email-preview-read';
+
+      document.querySelector('#emails-view').append(email_block);
+    });
+  });
 }
